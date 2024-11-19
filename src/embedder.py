647806d8a5da36bf92540e39c5b48abc99ca1bb1
Timothy Kostolansky <39891386+tim0120@users.noreply.github.com>
2024-11-19 15:12:16 -0800

    minimizing np imports

--- src/embedder.py
+++ src/embedder.py
@@ -1,5 +1,5 @@
 from typing import List
-import numpy as np
+from numpy import ndarray, load, dot, einsum, argsort
 from utils.inference import api_embed
 
 try:
@@ -23,7 +23,7 @@ class Embedder:
 		if load_locally:
 			self.tokenizer = AutoTokenizer.from_pretrained(model_path)
 			self.model = AutoModel.from_pretrained(model_path, trust_remote_code=True)
-		self.embeddings = np.load(
+		self.embeddings = load(
 			f'./data/embeddings/{model_path}/alternate_embeddings_quantized.npz',
 			allow_pickle=True
 		)['embeddings']
@@ -31,19 +31,19 @@ class Embedder:
 			with open('./data/emoji-info/characters.txt', 'r', encoding='utf-8') as file:
 				self.emoji_characters = file.read().splitlines()
 
-	def embed(self, input_texts: List[str]) -> np.ndarray:
+	def embed(self, input_texts: List[str]) -> ndarray:
 		if self.load_locally:
 			embeddings = local_embed(input_texts, self.tokenizer, self.model)
 		else:
 			embeddings = api_embed(input_texts, self.model_path, self.is_hf_model)
 		return embeddings
 
-	def k_nearest(self, queries: np.ndarray, k: int) -> List[int]:
+	def k_nearest(self, queries: ndarray, k: int) -> List[int]:
 		if len(self.embeddings.shape) == 2:
-			similarities = np.dot(self.embeddings, queries.T)
+			similarities = dot(self.embeddings, queries.T)
 		elif len(self.embeddings.shape) == 3:
-			similarities = np.einsum('ijk,lk->il', self.embeddings, queries)
+			similarities = einsum('ijk,lk->il', self.embeddings, queries)
 		else:
 			raise NotImplementedError('Only 2D and 3D embeddings are supported')
-		topk_indices = np.argsort(-similarities, axis=0)[:k]
+		topk_indices = argsort(-similarities, axis=0)[:k]
 		return topk_indices.flatten().tolist()
