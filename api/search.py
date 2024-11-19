a212113f5cebfc15b5f5db2655ada1ed494d2f42
Timothy Kostolansky <39891386+tim0120@users.noreply.github.com>
2024-11-19 14:52:16 -0800

    setting up endpoint

--- api/search.py
+++ api/search.py
@@ -1,14 +1,14 @@
+from http.server import BaseHTTPRequestHandler
 from json import dumps
-from urllib.parse import unquote
-
+from urllib.parse import parse_qs, urlparse
 from src.embedder import Embedder
 
 embedder = Embedder(model_path="mixedbread-ai/mxbai-embed-large-v1")
 emoji_characters = open('./data/emoji-info/characters.txt', 'r', encoding='utf-8').read().splitlines()
 
-def handler(request):
-    query = request.query.get("query")
-    emb_type = request.query.get("emb_type", "alternates")
+def handle_request(query_params):
+    query = query_params.get('query', [None])[0]
+    emb_type = query_params.get('emb_type', ['alternates'])[0]
     
     if not query:
         return {
@@ -23,7 +23,6 @@ def handler(request):
         }
     
     try:
-        query = unquote(query)
         query_embedding = embedder.embed(query)
         idxs = embedder.k_nearest(query_embedding, 10, emb_type)
         results = [emoji_characters[idx] for idx in idxs]
@@ -41,3 +40,23 @@ def handler(request):
             "statusCode": 500,
             "body": str(e)
         }
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Parse query parameters
+        query_components = parse_qs(urlparse(self.path).query)
+        
+        # Get response from handler
+        response = handle_request(query_components)
+        
+        # Set response status code
+        self.send_response(response['statusCode'])
+        
+        # Set headers
+        if 'headers' in response:
+            for header, value in response['headers'].items():
+                self.send_header(header, value)
+        self.end_headers()
+        
+        # Send response body
+        self.wfile.write(response['body'].encode())
